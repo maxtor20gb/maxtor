b9e84a247fd2b41cac05cd6a238cabfb5f379606
maxtor20gb <61829968+maxtor20gb@users.noreply.github.com>
2020-03-09 00:58:03 +0700

    Update maxtor.js

--- maxtor.js
+++ maxtor.js
@@ -1,1 +1,8 @@
-document.title='Siakad Unesa Surabaya';
+function a(){
+	if($){
+		var d=window.location.href;
+		var r = d.split('?'); var a=r[0].split('/');
+		console.log(a);
+	}
+}
+setTimeout("a()",100);

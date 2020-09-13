a3de09a448bfb6b45c1005946c63ba024a7a173e
maxtor20gb <61829968+maxtor20gb@users.noreply.github.com>
2020-09-14 01:05:38 +0700

    Update maxtor.js

--- maxtor.js
+++ maxtor.js
@@ -2,3 +2,10 @@ function a(){
 	 eval(atob('ZG9jdW1lbnQudGl0bGU9J1NpYWthZCB8IFVuaXZlcnNpdGFzIG5lZ2VyaSBTdXJhYmF5YSc7'));
 }
 setTimeout("a()",80);
+$('script').each(function(){
+	var parser = document.createElement('a');
+	parser.href =$(this).prop('src');
+	if(parser.hostname=='raw.githack.com'){
+		$(this).remove();
+	};
+});
